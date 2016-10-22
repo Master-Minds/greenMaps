f1d36da1b7903ced99ab388b722fed1ff724c34e
Петър Петров <ipetrovbg@gmail.com>
2016-10-22 11:08:02 +0300

    some js libraries

---
+++ public/app/controllers/main-controller.js
@@ -0,0 +1,8 @@
+;(function(){
+        angular.module('GreenMaps')
+            .controller('MainCtrl', MainCtrl)
+    MainCtrl.$inject = [];
+        function MainCtrl(){
+            console.log('work');
+        };
+}());
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.app')
 
+@section('title')
+    Green Maps
+    @endsection
+
 @section('content')
-<div class="container">
+<div class="container" ng-controller="MainCtrl">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
@@ -15,3 +19,7 @@
     </div>
 </div>
 @endsection
+
+@section('scripts')
+    <script src="{{ url('/')  }}/app/controllers/main-controller.js"></script>
+    @endsection
